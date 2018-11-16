7229b56158c34891c3ed7aadf7ab84b03e785282
Aleksey <sphota9@gmail.com>
2018-11-16 11:59:41 -0500

    simplify ProcessRequest

--- src/main/java/common/task/ProcessRequest.java
+++ src/main/java/common/task/ProcessRequest.java
@@ -1,22 +1,10 @@
 package common.task;
 
-import java.util.concurrent.Callable;
-
 public class ProcessRequest<T> {
 
     private String _destination;
     private String _source;
     private T _data;
-
-    public Callable<T> getTask() {
-        return _task;
-    }
-
-    public void setTask(Callable<T> _task) {
-        this._task = _task;
-    }
-
-    private Callable<T> _task;
 
     public ProcessRequest(String src, String dst, T data) {
         _destination = dst;
