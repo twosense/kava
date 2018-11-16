29eb5e129265899ef04fc61b681593418467863a
Aleksey <sphota9@gmail.com>
2018-11-16 11:46:10 -0500

    thread-safe main executor singleton

--- src/main/java/common/executor/TaskExecutor.java
+++ src/main/java/common/executor/TaskExecutor.java
@@ -3,16 +3,28 @@ package common.executor;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 
+import javax.annotation.Nullable;
 import java.util.concurrent.Executors;
 
 public class TaskExecutor {
 
-    private static final int MAX_THREAD_COUNT = 25;
-    private static ListeningExecutorService THREAD_POOL = MoreExecutors.listeningDecorator(
-        Executors.newFixedThreadPool(MAX_THREAD_COUNT)
-    );
+  private static final int MAX_THREAD_COUNT = 25;
+  private static final Object SINGLETON_LOCK = new Object();
+  @Nullable
+  private static volatile ListeningExecutorService sThreadPoolInstance = null;
 
-    public static ListeningExecutorService main() {
-        return THREAD_POOL;
+  public static ListeningExecutorService main() {
+    ListeningExecutorService instance = sThreadPoolInstance;
+    if (instance == null) {
+      synchronized (SINGLETON_LOCK) {
+        instance = sThreadPoolInstance;
+        if (instance == null) {
+          sThreadPoolInstance = instance = MoreExecutors.listeningDecorator(
+              Executors.newFixedThreadPool(MAX_THREAD_COUNT)
+          );
+        }
+      }
     }
+    return instance;
+  }
 }
